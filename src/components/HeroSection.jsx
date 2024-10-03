18dbc944d1571e286b208fe8d358d0135f06104a
jonayeds <jonayeeds@gmail.com>
2024-10-03 16:32:41 +0600

    added Hero section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div className='text-[17vw] font-heading px-[8vw] leading-[0.7] h-screen flex justify-center  flex-col' >
+        <div>
+        <div className='flex justify-start'><h1>APOLLINE</h1></div>
+        <div className='flex justify-end'><h1>ROBERT</h1></div>
+        <div className='flex justify-start'> <h1>STUDIO</h1></div>
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
