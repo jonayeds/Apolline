7a2a9ccd3e20e6df4e536147c11bdd6646d8e609
jonayeds <jonayeeds@gmail.com>
2024-10-03 17:00:36 +0600

    added hero section Transition animation

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,12 +1,31 @@
+"use client"
+import gsap from 'gsap'
 import React from 'react'
+import {useGSAP} from "@gsap/react"
 
 const HeroSection = () => {
+    useGSAP(()=>{
+        gsap.from(".heading1",{
+            x:-80,
+            duration:2,
+            opacity:0,
+            stagger:0.3,
+            ease: "circ.out",
+        },"head")
+        gsap.from(".heading2",{
+            x:80,
+            duration:2,
+            delay:0.1,
+            opacity:0,
+            ease: "circ.out",
+        }, "head")
+    })
   return (
     <div className='text-[17vw] font-heading px-[8vw] leading-[0.7] h-screen flex justify-center  flex-col' >
         <div>
-        <div className='flex justify-start'><h1>APOLLINE</h1></div>
-        <div className='flex justify-end'><h1>ROBERT</h1></div>
-        <div className='flex justify-start'> <h1>STUDIO</h1></div>
+        <div className='flex justify-start'><h1 className='heading1'>APOLLINE</h1></div>
+        <div className='flex justify-end'><h1 className='heading2'>ROBERT</h1></div>
+        <div className='flex justify-start'> <h1 className='heading1'>STUDIO</h1></div>
         </div>
     </div>
   )
